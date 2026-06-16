2a5d9438ef5e8900c2810451c13862ac1360bbc8
ShravaneeSK <kulkarnishravanee@gmail.com>
2026-06-17 00:03:46 +0530

    Time: 3 ms (17.73%), Space: 8.5 MB (86.19%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,10 +1,27 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        long long i = 1;
-        while(i*i<=x){
-            i++;
-        }
-        return i-1;
+       if(x==0 || x==1)
+       return x;
+
+       int start = 1;
+       int end =x;
+       int mid = -1;
+
+       while(start<=end){
+        mid=start+(end-start)/2;
+
+        long long square = static_cast<long long>(mid)*mid;
+
+        if(square > x)
+        end=mid-1;
+        else if(square==x)
+        return mid;
+        else
+        start=mid+1;
+
+
+       }
+       return static_cast<int>(std::round(end));
     }
 };
